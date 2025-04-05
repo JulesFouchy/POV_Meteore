30e2709477e6ad5e66fcbb80e88ca574017e234f
Jules Fouchy <jules.fouchy@ntymail.com>
2025-04-05 23:24:54 +0200

    Revert "Add fmod plugin part 2"
    This reverts commit 65cebcb81ebff7bb629bc9543825bfa4ed6bb967.

--- Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/PCH.FMODStudio.h
+++
@@ -1,3 +0,0 @@
-// PCH for C:/Dev/Unreal/POV_Meteore/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h
-#include "C:/Dev/Unreal/POV_Meteore/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/Definitions.h"
-#include "C:/Dev/Unreal/POV_Meteore/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h"
